b157907a352037614b910931f09d3b068b6d3f36
Anton Safonov <anton.safonov@gmail.com>
2011-02-03 15:46:11 +0200

    temporarily switching subversive off

--- com.cloudbees.eclipse.core/src/com/cloudbees/eclipse/core/internal/forge/ForgeSubversiveSync.java
+++ com.cloudbees.eclipse.core/src/com/cloudbees/eclipse/core/internal/forge/ForgeSubversiveSync.java
@@ -3,9 +3,9 @@ package com.cloudbees.eclipse.core.internal.forge;
 import java.util.Properties;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.team.svn.core.resource.IRepositoryLocation;
-import org.eclipse.team.svn.core.svnstorage.SVNRemoteStorage;
-import org.eclipse.team.svn.core.utility.SVNUtility;
+//import org.eclipse.team.svn.core.resource.IRepositoryLocation;
+//import org.eclipse.team.svn.core.svnstorage.SVNRemoteStorage;
+//import org.eclipse.team.svn.core.utility.SVNUtility;
 
 import com.cloudbees.eclipse.core.CloudBeesException;
 
@@ -23,17 +23,17 @@ public class ForgeSubversiveSync implements ForgeSync {
       throw new IllegalArgumentException("url not provided!");
     }
 
-    IRepositoryLocation loc = SVNRemoteStorage.instance().newRepositoryLocation();
-    monitor.setTaskName("Validating SVN repository connection.");
-
-    Exception ex = SVNUtility.validateRepositoryLocation(loc);
-    if (ex != null) {
-      throw new CloudBeesException("Failed to validate SVN connection to " + url, ex);
-    }
-
-    monitor.setTaskName("Adding repository.");
-    SVNRemoteStorage.instance().addRepositoryLocation(loc);
-    monitor.done();
+//    IRepositoryLocation loc = SVNRemoteStorage.instance().newRepositoryLocation();
+//    monitor.setTaskName("Validating SVN repository connection.");
+//
+//    Exception ex = SVNUtility.validateRepositoryLocation(loc);
+//    if (ex != null) {
+//      throw new CloudBeesException("Failed to validate SVN connection to " + url, ex);
+//    }
+//
+//    monitor.setTaskName("Adding repository.");
+//    SVNRemoteStorage.instance().addRepositoryLocation(loc);
+//    monitor.done();
   }
 
 }
