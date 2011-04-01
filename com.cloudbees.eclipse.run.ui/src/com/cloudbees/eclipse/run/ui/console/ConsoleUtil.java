a8783628d3bd6d9ec756dc85d063c00a986f573e
Innar Made <imade@codehoop.com>
2011-04-01 15:01:54 +0300

    Working with console util.

--- com.cloudbees.eclipse.run.ui/src/com/cloudbees/eclipse/run/ui/console/ConsoleUtil.java
+++ com.cloudbees.eclipse.run.ui/src/com/cloudbees/eclipse/run/ui/console/ConsoleUtil.java
@@ -1,13 +1,26 @@
 package com.cloudbees.eclipse.run.ui.console;
 
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
 import org.eclipse.ui.console.MessageConsole;
 
 public class ConsoleUtil {
-  
+
   // TODO add support for eclipse console
-  
+
   public static MessageConsole getOrCreateConsole(String consoleName) {
-    return null;
+    IConsoleManager consoleManager = ConsolePlugin.getDefault().getConsoleManager();
+
+    for (IConsole console : consoleManager.getConsoles()) {
+      if (consoleName.equals(console.getName())) {
+        return (MessageConsole) console;
+      }
+    }
+
+    MessageConsole newConsole = new MessageConsole(consoleName, null);
+    consoleManager.addConsoles(new IConsole[] { newConsole });
+    return newConsole;
   }
-  
+
 }
