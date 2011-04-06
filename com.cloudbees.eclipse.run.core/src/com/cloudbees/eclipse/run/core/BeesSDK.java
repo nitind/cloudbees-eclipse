e4da3b0fd2e99f063e421217b2f0f4d486a091dd
Ivar Meikas <imeikas@gmail.com>
2011-04-06 14:11:37 +0300

    Now using cloudbees opensource API to deploy to cloud

--- com.cloudbees.eclipse.run.core/src/com/cloudbees/eclipse/run/core/BeesSDK.java
+++ com.cloudbees.eclipse.run.core/src/com/cloudbees/eclipse/run/core/BeesSDK.java
@@ -1,11 +1,18 @@
 package com.cloudbees.eclipse.run.core;
 
+import java.io.FileNotFoundException;
+
 import org.apache.tools.ant.listener.TimestampedLogger;
 import org.eclipse.ant.core.AntRunner;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 
+import com.cloudbees.api.ApplicationDeployArchiveResponse;
+import com.cloudbees.api.BeesClient;
+import com.cloudbees.api.BeesClientConfiguration;
 import com.cloudbees.eclipse.core.CloudBeesCorePlugin;
 import com.cloudbees.eclipse.core.CloudBeesException;
 import com.cloudbees.eclipse.core.GrandCentralService;
@@ -14,12 +21,59 @@ import com.cloudbees.eclipse.sdk.CBSdkActivator;
 
 public class BeesSDK {
 
-  public void deploy(IProject project) throws Exception {
-    runTargets(project, new String[] { "deploy" });
+  public static final String API_URL = "https://api.cloudbees.com/api";
+
+  public ApplicationDeployArchiveResponse deploy(IProject project) throws Exception {
+    GrandCentralService grandCentralService = CloudBeesCorePlugin.getDefault().getGrandCentralService();
+    BeesClient client = getBeesClient(grandCentralService);
+
+    String appId = grandCentralService.getCachedPrimaryUser(false) + "/" + project.getName();//$NON-NLS-1$
+
+    IPath workspacePath = project.getLocation().removeLastSegments(1);
+    IPath buildPath = getWarFile(project).getFullPath();
+
+    String warFile = workspacePath.toOSString() + buildPath.toOSString();
+    return client.applicationDeployWar(appId, null, null, warFile, null, null);
   }
 
-  public void runLocal(IProject project) throws Exception {
-    runTargets(project, new String[] { "run" });
+  private IFile getWarFile(IProject project) throws CloudBeesException, CoreException, FileNotFoundException {
+    IFile file = getBuildFolder(project).getFile("webapp.war");
+
+    if (!file.exists()) {
+      runTargets(project, new String[] { "dist" });
+
+      if (!file.exists()) {
+        throw new FileNotFoundException("Could not find webapp.war file in build folder .");
+      }
+    }
+
+    return file;
+  }
+
+  private IFolder getBuildFolder(IProject project) throws CloudBeesException, CoreException, FileNotFoundException {
+
+    IFolder folder = project.getFolder("build");
+    if (!folder.exists()) {
+      runTargets(project, new String[] { "dist" });
+
+      if (!folder.exists()) {
+        throw new FileNotFoundException(
+            "Unexpected project structure. Could not find folder \"build\" in project root.");
+      }
+    }
+
+    return folder;
+  }
+
+  private BeesClient getBeesClient(GrandCentralService grandCentralService) throws CloudBeesException {
+    AuthInfo cachedAuthInfo = grandCentralService.getCachedAuthInfo(false);
+
+    String api_key = cachedAuthInfo.getAuth().api_key;
+    String secret_key = cachedAuthInfo.getAuth().secret_key;
+
+    BeesClientConfiguration conf = new BeesClientConfiguration(API_URL, api_key, secret_key, "xml", "1.0");
+    BeesClient client = new BeesClient(conf);
+    return client;
   }
 
   private void runTargets(IProject project, String[] targets) throws CloudBeesException, CoreException {
@@ -45,7 +99,7 @@ public class BeesSDK {
     AntRunner runner = new AntRunner();
 
     runner.setBuildFileLocation(getBuildXmlPath(project));
-    runner.setExecutionTargets( new String[] { "run" });
+    runner.setExecutionTargets(new String[] { "run" });
 
     GrandCentralService grandCentralService = CloudBeesCorePlugin.getDefault().getGrandCentralService();
     AuthInfo cachedAuthInfo = grandCentralService.getCachedAuthInfo(false);
@@ -59,7 +113,7 @@ public class BeesSDK {
     runner.addBuildLogger(TimestampedLogger.class.getName());
     return runner;
   }
-  
+
   /**
    * Construct full path for the build.xml
    * 
