e0f5ba56d7c04aa0549e4891c6774474b5fcaa1d
Innar Made <imade@codehoop.com>
2011-04-27 15:16:39 +0300

    Improved cloudbees perspective.

--- com.cloudbees.eclipse.run.ui/src/com/cloudbees/eclipse/run/ui/Images.java
+++ com.cloudbees.eclipse.run.ui/src/com/cloudbees/eclipse/run/ui/Images.java
@@ -1,10 +1,11 @@
 package com.cloudbees.eclipse.run.ui;
 
+
 public interface Images {
-  
+
   String CLOUDBEES_WIZ_ICON_PATH = "icons/cb_wiz_icon.png";
-  
+
   String CLOUDBEES_ICON_16x16_PATH = "icons/16x16/cb_plain.png";
   String CLOUDBEES_ICON_16x16 = Images.class.getSimpleName() + ".cb_plain.png";
-  
+
 }
