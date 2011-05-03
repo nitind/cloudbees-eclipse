eb18514686417d2eed9570bc35a4e0fe34021150
Anton Safonov <anton.safonov@gmail.com>
2011-05-03 18:20:55 +0300

    ﻿added war and app selection dialog for deploy

--- com.cloudbees.eclipse.dev.ui/src/com/cloudbees/eclipse/dev/ui/views/build/ArtifactsClickListener.java
+++ com.cloudbees.eclipse.dev.ui/src/com/cloudbees/eclipse/dev/ui/views/build/ArtifactsClickListener.java
@@ -5,12 +5,15 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.TreeSelection;
@@ -18,7 +21,6 @@ import org.eclipse.ui.PlatformUI;
 
 import com.cloudbees.api.ApplicationDeployArchiveResponse;
 import com.cloudbees.api.ApplicationInfo;
-import com.cloudbees.api.ApplicationListResponse;
 import com.cloudbees.eclipse.core.JenkinsService;
 import com.cloudbees.eclipse.core.forge.api.ForgeSync.ArtifactPathItem;
 import com.cloudbees.eclipse.core.jenkins.api.JenkinsBuildDetailsResponse;
@@ -40,74 +42,103 @@ public class ArtifactsClickListener implements IDoubleClickListener {
     final JenkinsBuildDetailsResponse build = item.parent;
     final Artifact artifact = item.item;
 
-    final String warUrl = getWarUrl(build, artifact);
-    if (warUrl == null) {
-      return; // not war item
+    if (!artifact.relativePath.endsWith(".war")) {
+      return;
     }
 
-    deployWar(build, warUrl);
+    deployWar(build, artifact);
   }
 
   protected static String getWarUrl(final JenkinsBuildDetailsResponse build, final Artifact artifact) {
-    if (!artifact.relativePath.endsWith(".war")) {
+    String warPath = artifact.relativePath;
+    if (!warPath.endsWith(".war")) {
       return null;
     }
 
-    final String warUrl = build.url + "artifact/" + artifact.relativePath;
+    final String warUrl = build.url + "artifact/" + warPath;
     return warUrl;
   }
 
-  public static void deployWar(final JenkinsBuildDetailsResponse build, String warUrl) {
-    final String appId;
+  public static void deployWar(final JenkinsBuildDetailsResponse build, final Artifact selectedWar) {
+    final Artifact war;
+    final ApplicationInfo app;
 
-    { // TODO show war & app selector
-      try {
-        if (warUrl == null) {
-          if (build.artifacts == null) {
-            return;
-          }
-          for (Artifact art : build.artifacts) {
-            warUrl = getWarUrl(build, art);
-            if (warUrl != null) {
-              break; // found some war
-            }
-          }
-        }
-        if (warUrl == null) {
-          return;
-        }
-
-        ApplicationListResponse list = BeesSDK.getList();
-        ApplicationInfo first = list.getApplications().iterator().next();
-        appId = first.getId();
-        boolean confirm = MessageDialog.openConfirm(CloudBeesUIPlugin.getActiveWindow().getShell(), "Artifact WAR",
-            "War: " + warUrl + " to " + appId);
-        if (!confirm) {
-          return;
-        }
-      } catch (Exception e) {
-        e.printStackTrace(); // TODO report to user?
-        return;
-      }
+    if (build.artifacts == null) {
+      return;
     }
 
-    final String warUrl_ = warUrl;
-    org.eclipse.core.runtime.jobs.Job job = new org.eclipse.core.runtime.jobs.Job("Deploying Jenkins war artifact...") {
+    final DeployWarAppDialog[] selector = new DeployWarAppDialog[1];
+    PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+      @Override
+      public void run() {
+        final List<Artifact> wars = new ArrayList<Artifact>();
+        final List<ApplicationInfo> apps = new ArrayList<ApplicationInfo>();
+        try {
+          PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress() {
+            @Override
+            public void run(final IProgressMonitor monitor) {
+              try {
+                for (Artifact art : build.artifacts) {
+                  if (art.relativePath != null && art.relativePath.endsWith(".war")) {
+                    wars.add(art);
+                    monitor.worked(1);
+                  }
+                }
+
+                apps.addAll(BeesSDK.getList().getApplications());
+                monitor.worked(1);
+              } catch (Exception e) {
+                CloudBeesDevUiPlugin.getDefault().getLogger().error(e);
+              }
+            }
+          });
+        } catch (InterruptedException e) {
+          return;
+        } catch (Exception e) {
+          CloudBeesDevUiPlugin.getDefault().getLogger().error(e);
+        }
+
+        if (wars.isEmpty() || apps.isEmpty()) {
+          MessageDialog.openInformation(CloudBeesUIPlugin.getActiveWindow().getShell(), "Deploy to RUN@cloud",
+              "Deployment is not possible.");
+          return;
+        }
+
+        selector[0] = new DeployWarAppDialog(CloudBeesUIPlugin.getActiveWindow().getShell(), wars, selectedWar, apps);
+        selector[0].open();
+      }
+    });
+
+    if (selector[0] == null) {
+      return;
+    }
+
+    war = selector[0].getSelectedWar();
+    app = selector[0].getSelectedApp();
+
+    if (war == null || app == null) {
+      return; // cancelled
+    }
+
+    org.eclipse.core.runtime.jobs.Job job = new org.eclipse.core.runtime.jobs.Job("Deploy war to RUN@cloud") {
       @Override
       protected IStatus run(final IProgressMonitor monitor) {
         try {
-          JenkinsService service = CloudBeesUIPlugin.getDefault().getJenkinsServiceForUrl(warUrl_);
-          BufferedInputStream in = new BufferedInputStream(service.getArtifact(warUrl_, monitor));
+          String warUrl = getWarUrl(build, war);
+          JenkinsService service = CloudBeesUIPlugin.getDefault().getJenkinsServiceForUrl(warUrl);
+          BufferedInputStream in = new BufferedInputStream(service.getArtifact(warUrl, monitor));
 
-          String warName = warUrl_.substring(warUrl_.lastIndexOf("/"));
+          String warName = warUrl.substring(warUrl.lastIndexOf("/"));
           final File tempWar = File.createTempFile(warName, null);
           tempWar.deleteOnExit();
 
+          monitor.beginTask("Downloading war file...", 100);
           OutputStream out = new BufferedOutputStream(new FileOutputStream(tempWar));
           byte[] buf = new byte[1 << 12];
           int len = 0;
           while ((len = in.read(buf)) >= 0) {
             out.write(buf, 0, len);
+            monitor.worked(1);
           }
 
           out.flush();
@@ -116,34 +147,35 @@ public class ArtifactsClickListener implements IDoubleClickListener {
 
           final String[] newAppUrl = new String[1];
           try {
-            ApplicationDeployArchiveResponse result = BeesSDK.deploy(appId, tempWar.getCanonicalPath());
+            monitor.beginTask("Deploying war file to RUN@cloud...", 100);
+            monitor.worked(50);
+            ApplicationDeployArchiveResponse result = BeesSDK.deploy(app.getId(), tempWar.getCanonicalPath());
+            monitor.worked(50);
             newAppUrl[0] = result.getUrl();
           } catch (Exception e) {
-            e.printStackTrace();
-            // TODO report
+            return new Status(IStatus.ERROR, CloudBeesDevUiPlugin.PLUGIN_ID, e.getMessage(), e);
+          } finally {
+            monitor.done();
           }
 
           PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
             @Override
             public void run() {
               try {
-                monitor.done();
-
                 if (newAppUrl[0] != null) {
                   boolean openConfirm = MessageDialog.openConfirm(CloudBeesUIPlugin.getActiveWindow().getShell(),
-                      "Deploy to RUN@cloud", "Deployment succeeded to RUN@cloud '" + appId + "'.\nOpen " + newAppUrl[0]
-                          + "?");
+                      "Deploy to RUN@cloud", "Deployment succeeded to RUN@cloud '" + app.getId() + "'.\nOpen "
+                          + newAppUrl[0] + " in the browser?");
 
                   if (openConfirm) {
                     CloudBeesUIPlugin.getDefault().openWithBrowser(newAppUrl[0]);
                   }
                 } else {
                   MessageDialog.openWarning(CloudBeesUIPlugin.getActiveWindow().getShell(), "Deploy to RUN@cloud",
-                      "Deployment failed to RUN@cloud '" + appId + "'.");
+                      "Deployment failed to RUN@cloud '" + app.getId() + "'.");
                 }
               } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+                CloudBeesDevUiPlugin.getDefault().getLogger().error(e);
               }
             }
           });
@@ -163,5 +195,4 @@ public class ArtifactsClickListener implements IDoubleClickListener {
     job.setUser(true);
     job.schedule();
   }
-
 }
---
+++ com.cloudbees.eclipse.dev.ui/src/com/cloudbees/eclipse/dev/ui/views/build/DeployWarAppDialog.java
@@ -0,0 +1,132 @@
+package com.cloudbees.eclipse.dev.ui.views.build;
+
+import java.util.List;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+import com.cloudbees.api.ApplicationInfo;
+import com.cloudbees.eclipse.core.jenkins.api.JenkinsBuildDetailsResponse.Artifact;
+
+public class DeployWarAppDialog extends Dialog {
+
+  protected Combo comboWar;
+  protected Combo comboApp;
+
+  protected List<Artifact> wars;
+  protected List<ApplicationInfo> apps;
+
+  protected Artifact selectedWar;
+  protected ApplicationInfo selectedApp;
+
+  /**
+   * Create the dialog.
+   * @param parentShell
+   */
+  public DeployWarAppDialog(final Shell parentShell, final List<Artifact> wars, final Artifact selectedWar,
+      final List<ApplicationInfo> apps) {
+    super(parentShell);
+    this.wars = wars;
+    this.apps = apps;
+    this.selectedWar = selectedWar;
+  }
+
+  /**
+   * Create contents of the dialog.
+   * @param parent
+   */
+  @Override
+  protected Control createDialogArea(final Composite parent) {
+    Composite container = (Composite) super.createDialogArea(parent);
+    GridLayout gridLayout = (GridLayout) container.getLayout();
+
+    Label labelIntro = new Label(container, SWT.NONE);
+    labelIntro.setText("Select war and RUN@cloud application to deploy war to.");
+    new Label(container, SWT.NONE);
+
+    Label labelWar = new Label(container, SWT.NONE);
+    labelWar.setText("Available war artifacts:");
+
+    this.comboWar = new Combo(container, SWT.NONE);
+    this.comboWar.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+    new Label(container, SWT.NONE);
+
+    Label labelApp = new Label(container, SWT.NONE);
+    labelApp.setText("Available RUN@cloud applications:");
+
+    this.comboApp = new Combo(container, SWT.NONE);
+    this.comboApp.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+    initCombos();
+
+    return container;
+  }
+
+  public void initCombos() {
+    int tally = 0;
+    for (Artifact war : this.wars) {
+      this.comboWar.add(war.displayPath);
+      if (tally == 0 || war == this.selectedWar) {
+        this.comboWar.select(tally);
+      }
+      ++tally;
+    }
+
+    tally = 0;
+    for (ApplicationInfo app : this.apps) {
+      this.comboApp.add(app.getId());
+      if (tally == 0) {
+        this.comboApp.select(tally);
+      }
+    }
+  }
+
+  /**
+   * Create contents of the button bar.
+   * @param parent
+   */
+  @Override
+  protected void createButtonsForButtonBar(final Composite parent) {
+    createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+    createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+  }
+
+  /**
+   * Return the initial size of the dialog.
+   */
+  @Override
+  protected Point getInitialSize() {
+    return new Point(450, 300);
+  }
+
+  @Override
+  protected void okPressed() {
+    this.selectedWar = this.wars.get(this.comboWar.getSelectionIndex());
+    this.selectedApp = this.apps.get(this.comboApp.getSelectionIndex());
+    super.okPressed();
+  }
+
+  @Override
+  protected void cancelPressed() {
+    this.selectedWar = null;
+    this.selectedApp = null;
+    super.cancelPressed();
+  }
+
+  public Artifact getSelectedWar() {
+    return this.selectedWar;
+  }
+
+  public ApplicationInfo getSelectedApp() {
+    return this.selectedApp;
+  }
+}
