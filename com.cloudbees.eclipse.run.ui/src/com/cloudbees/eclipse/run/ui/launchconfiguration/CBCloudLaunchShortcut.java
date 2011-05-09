5e4baa9cc12928a770ec539e6c7794ce85fac60d
Ivar Meikas <imeikas@gmail.com>
2011-05-09 19:41:23 +0300

    Fixed an NPE for deploy

--- com.cloudbees.eclipse.run.ui/src/com/cloudbees/eclipse/run/ui/launchconfiguration/CBCloudLaunchShortcut.java
+++ com.cloudbees.eclipse.run.ui/src/com/cloudbees/eclipse/run/ui/launchconfiguration/CBCloudLaunchShortcut.java
@@ -16,9 +16,9 @@ import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.PlatformUI;
 
 import com.cloudbees.eclipse.run.core.launchconfiguration.CBLaunchConfigurationConstants;
 import com.cloudbees.eclipse.run.core.util.CBRunUtil;
@@ -26,46 +26,64 @@ import com.cloudbees.eclipse.run.ui.CBRunUiActivator;
 
 public class CBCloudLaunchShortcut implements ILaunchShortcut {
 
+  private ILaunchConfiguration configuration;
+  private String name;
+  boolean cancelled = false;
+
   @Override
   public void launch(ISelection selection, String mode) {
     if (selection instanceof IStructuredSelection) {
 
       IStructuredSelection structuredSelection = (IStructuredSelection) selection;
 
-      String name = null;
       Object element = structuredSelection.getFirstElement();
 
       if (element instanceof IProject) {
-        name = ((IProject) element).getName();
+        this.name = ((IProject) element).getName();
       } else if (element instanceof IJavaProject) {
-        name = ((IJavaProject) element).getProject().getName();
+        this.name = ((IJavaProject) element).getProject().getName();
       }
 
       try {
-        List<ILaunchConfiguration> launchConfigurations = CBRunUtil.getOrCreateCloudBeesLaunchConfigurations(name,
+        List<ILaunchConfiguration> launchConfigurations = CBRunUtil.getOrCreateCloudBeesLaunchConfigurations(this.name,
             true, null);
-        ILaunchConfiguration configuration = launchConfigurations.get(launchConfigurations.size() - 1);
+        this.configuration = launchConfigurations.get(launchConfigurations.size() - 1);
 
-        String accountName = configuration.getAttribute(CBLaunchConfigurationConstants.ATTR_CB_LAUNCH_ACCOUNT_ID, "");
+        String accountName = this.configuration.getAttribute(CBLaunchConfigurationConstants.ATTR_CB_LAUNCH_ACCOUNT_ID,
+            "");
         if (accountName.equals("")) {
-          Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-          AccountSelectionDialog dialog = new AccountSelectionDialog(shell);
-          dialog.open();
-          String account = dialog.getSelectedAccountName();
+          Display.getDefault().syncExec(new Runnable() {
 
-          if (dialog.getReturnCode() != IDialogConstants.OK_ID || account == null || account.length() == 0) {
-            String errorMsg = MessageFormat.format("Account is not specified.", name);
-            Status status = new Status(IStatus.ERROR, CBRunUiActivator.PLUGIN_ID, errorMsg);
-            ErrorDialog.openError(shell, "Error", "Launch error", status);
-            return;
-          }
+            @Override
+            public void run() {
+              try {
+                Shell shell = Display.getDefault().getActiveShell();
+                AccountSelectionDialog dialog = new AccountSelectionDialog(shell);
+                dialog.open();
+                String account = dialog.getSelectedAccountName();
 
-          ILaunchConfigurationWorkingCopy copy = configuration.getWorkingCopy();
-          copy.setAttribute(CBLaunchConfigurationConstants.ATTR_CB_LAUNCH_ACCOUNT_ID, account);
-          configuration = copy.doSave();
+                if (dialog.getReturnCode() != IDialogConstants.OK_ID || account == null || account.length() == 0) {
+                  String errorMsg = MessageFormat.format("Account is not specified.", CBCloudLaunchShortcut.this.name);
+                  Status status = new Status(IStatus.ERROR, CBRunUiActivator.PLUGIN_ID, errorMsg);
+                  ErrorDialog.openError(shell, "Error", "Launch error", status);
+                  CBCloudLaunchShortcut.this.cancelled = true;
+                  return;
+                }
+                CBCloudLaunchShortcut.this.cancelled = false;
+
+                ILaunchConfigurationWorkingCopy copy = CBCloudLaunchShortcut.this.configuration.getWorkingCopy();
+                copy.setAttribute(CBLaunchConfigurationConstants.ATTR_CB_LAUNCH_ACCOUNT_ID, account);
+                CBCloudLaunchShortcut.this.configuration = copy.doSave();
+              } catch (CoreException e) {
+                CBRunUiActivator.logError(e);
+              }
+            }
+          });
         }
 
-        DebugUITools.launch(configuration, mode);
+        if (!this.cancelled) {
+          DebugUITools.launch(this.configuration, mode);
+        }
       } catch (CoreException e) {
         CBRunUiActivator.logError(e);
       }
