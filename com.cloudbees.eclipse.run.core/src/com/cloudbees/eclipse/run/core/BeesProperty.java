1b455668f346a874c64501b78ff0ce3fddd215a4
Ahti Kitsik <ahti@codehoop.com>
2012-09-06 14:39:16 +0300

    Added placeholder code for potential future change where one could use native ant scripts to run bees sdk tasks.

---
+++ com.cloudbees.eclipse.run.core/src/com/cloudbees/eclipse/run/core/BeesProperty.java
@@ -0,0 +1,46 @@
+package com.cloudbees.eclipse.run.core;
+
+import org.eclipse.ant.core.IAntPropertyValueProvider;
+
+import com.cloudbees.eclipse.core.CloudBeesCorePlugin;
+import com.cloudbees.eclipse.core.CloudBeesException;
+import com.cloudbees.eclipse.core.GrandCentralService;
+import com.cloudbees.eclipse.core.GrandCentralService.AuthInfo;
+import com.cloudbees.eclipse.run.sdk.CBSdkActivator;
+
+public class BeesProperty implements IAntPropertyValueProvider {
+
+  public String getAntPropertyValue(String prop) {
+    if ("bees.home".equalsIgnoreCase(prop)) {
+      return CBSdkActivator.getDefault().getBeesHome();
+    }
+
+    /*    if ("bees.apiSecret".equalsIgnoreCase(prop)) {
+          AuthInfo auth = getAuth();
+          if (auth == null) {
+            return null;
+          }
+          return auth.getAuth().secret_key;
+        }
+
+        if ("bees.apiKey".equalsIgnoreCase(prop)) {
+          AuthInfo auth = getAuth();
+          if (auth == null) {
+            return null;
+          }
+          return auth.getAuth().api_key;
+        }
+    */
+    return null;
+  }
+
+  private AuthInfo getAuth() {
+    GrandCentralService grandCentralService;
+    try {
+      grandCentralService = CloudBeesCorePlugin.getDefault().getGrandCentralService();
+      return grandCentralService.getCachedAuthInfo(false);
+    } catch (CloudBeesException e) {
+      return null;
+    }
+  }
+}
