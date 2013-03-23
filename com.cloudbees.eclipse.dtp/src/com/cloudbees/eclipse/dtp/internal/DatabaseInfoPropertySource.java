8cd45c701a04c9b286d5bfffab22ea86703110b9
Ahti Kitsik <ahti@codehoop.com>
2013-03-23 21:15:09 +0200

    Added show password menuitem.

--- com.cloudbees.eclipse.dtp/src/com/cloudbees/eclipse/dtp/internal/DatabaseInfoPropertySource.java
+++ com.cloudbees.eclipse.dtp/src/com/cloudbees/eclipse/dtp/internal/DatabaseInfoPropertySource.java
@@ -27,12 +27,13 @@ import com.cloudbees.api.DatabaseInfo;
 public class DatabaseInfoPropertySource implements IPropertySource {
 
   private static final String BASE = DatabaseInfoPropertySource.class.getSimpleName().toLowerCase();
-  private static final String PROPRTY_NAME = BASE + ".id";
-  private static final String PROPRTY_MASTER = BASE + ".title";
-  private static final String PROPRTY_OWNER = BASE + ".owner";
-  private static final String PROPRTY_USERNAME = BASE + ".username";
-  private static final String PROPRTY_STATUS = BASE + ".status";
-  private static final String PROPRTY_CREATED_DATE = BASE + ".created.date";
+  private static final String PROPERTY_NAME = BASE + ".id";
+  private static final String PROPERTY_MASTER = BASE + ".title";
+  private static final String PROPERTY_OWNER = BASE + ".owner";
+  private static final String PROPERTY_USERNAME = BASE + ".username";
+  private static final String PROPERTY_STATUS = BASE + ".status";
+  private static final String PROPERTY_CREATED_DATE = BASE + ".created.date";
+  //private static final String PROPERTY_PASSWORD = BASE + ".password";
 
   private IPropertyDescriptor[] propertyDescriptors;
   private final DatabaseInfo dbInfo;
@@ -50,15 +51,15 @@ public class DatabaseInfoPropertySource implements IPropertySource {
   public IPropertyDescriptor[] getPropertyDescriptors() {
     if (this.propertyDescriptors == null) {
 
-      PropertyDescriptor nameDescriptor = new PropertyDescriptor(PROPRTY_NAME, "Database Name");
+      PropertyDescriptor nameDescriptor = new PropertyDescriptor(PROPERTY_NAME, "Database Name");
 
-      PropertyDescriptor masterDescriptor = new PropertyDescriptor(PROPRTY_MASTER, "Master");
-      PropertyDescriptor ownerDescriptor = new PropertyDescriptor(PROPRTY_OWNER, "Owner");
-      PropertyDescriptor usernameDescriptor = new PropertyDescriptor(PROPRTY_USERNAME, "Username");
+      PropertyDescriptor masterDescriptor = new PropertyDescriptor(PROPERTY_MASTER, "Master");
+      PropertyDescriptor ownerDescriptor = new PropertyDescriptor(PROPERTY_OWNER, "Owner");
+      PropertyDescriptor usernameDescriptor = new PropertyDescriptor(PROPERTY_USERNAME, "Username");
 
-      PropertyDescriptor statusDescriptor = new PropertyDescriptor(PROPRTY_STATUS, "Status");
+      PropertyDescriptor statusDescriptor = new PropertyDescriptor(PROPERTY_STATUS, "Status");
 
-      PropertyDescriptor createdDateDescriptor = new PropertyDescriptor(PROPRTY_CREATED_DATE, "Created Date");
+      PropertyDescriptor createdDateDescriptor = new PropertyDescriptor(PROPERTY_CREATED_DATE, "Created Date");
       createdDateDescriptor.setLabelProvider(new LabelProvider() {
         @Override
         public String getText(Object element) {
@@ -70,8 +71,25 @@ public class DatabaseInfoPropertySource implements IPropertySource {
         }
       });
 
+/*      PropertyDescriptor passwordDescriptor = new PropertyDescriptor(PROPERTY_PASSWORD, "Password") {
+        
+        @Override
+        public CellEditor createPropertyEditor(Composite parent) {
+          return new TextCellEditor(parent) {
+            
+          };
+        }
+      };     
+      
+      passwordDescriptor.setLabelProvider(new LabelProvider() {
+        public String getText(Object element) {
+          return "..." + element;
+        }
+      });
+*/
+      
       this.propertyDescriptors = new IPropertyDescriptor[] { nameDescriptor, ownerDescriptor, usernameDescriptor, masterDescriptor, statusDescriptor,
-          createdDateDescriptor};
+          createdDateDescriptor/*, passwordDescriptor*/};
     }
 
     return this.propertyDescriptors;
@@ -79,31 +97,34 @@ public class DatabaseInfoPropertySource implements IPropertySource {
 
   @Override
   public Object getPropertyValue(Object id) {
-    if (id.equals(PROPRTY_NAME)) {
+    if (id.equals(PROPERTY_NAME)) {
       return this.dbInfo.getName();
     }
 
-    if (id.equals(PROPRTY_MASTER)) {      
+    if (id.equals(PROPERTY_MASTER)) {      
       return this.dbInfo.getMaster();
     }
 
-    if (id.equals(PROPRTY_OWNER)) {
+    if (id.equals(PROPERTY_OWNER)) {
       return this.dbInfo.getOwner();
     }
 
-    if (id.equals(PROPRTY_USERNAME)) {
+    if (id.equals(PROPERTY_USERNAME)) {
       return this.dbInfo.getUsername();
     }
 
-    if (id.equals(PROPRTY_STATUS)) {
+    if (id.equals(PROPERTY_STATUS)) {
       return this.dbInfo.getStatus();
     }
 
-    if (id.equals(PROPRTY_CREATED_DATE)) {
+    if (id.equals(PROPERTY_CREATED_DATE)) {
       return this.dbInfo.getCreated();
     }
 
-
+/*    if (id.equals(PROPERTY_PASSWORD)) {
+      return this.dbInfo.getPassword();
+    }
+*/
     return null;
   }
 
